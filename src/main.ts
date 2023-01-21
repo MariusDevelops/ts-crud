ac05f745d3243aebc9ac287dddc4b8891acfab2a
MariusDevelops <brazasmarius@gmail.com>
2023-01-21 02:15:45 +0200

    feat: CarsCollection constructor, joinCar and get all methods

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
-import App from './components/app';
+import CarsCollection from './helpers/cars-collection';
+import cars from './data/cars';
+import brands from './data/brands';
+import models from './data/models';
 
-const app = new App('#root');
-app.initialize();
+// import App from './components/app';
+
+// 1.2.1. Gautus duomenis išsaugokite objekte
+const carsCollection = new CarsCollection({
+    cars,
+    brands,
+    models,
+});
+
+console.log(carsCollection);
+// 1.2.2. Viena apjungta mašina
+console.log(carsCollection.joinCar(cars[0]));
+// 1.2.3. Gauti visas mašinas
+console.log(carsCollection.all);
+
+// const app = new App('#root');
+// app.initialize();
